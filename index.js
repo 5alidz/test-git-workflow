343869bb54c82afd4e39c9be8d28137c667d77cb
khaled <kzack-dev@hotmail.com>
2021-03-02 16:03:29 +0300

    feat: add lazy logging via sleep fn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function log(msg) {
-  console.log(msg);
+function sleep(time) {
+  return new Promise((res) => setTimeout(res, time));
+}
+
+function log(msg, time) {
+  if (time) {
+    sleep(time).then(() => console.log(msg));
+  } else {
+    console.log(msg);
+  }
 }
 
 module.exports = {
