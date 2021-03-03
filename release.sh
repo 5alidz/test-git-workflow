943cd6bbd8fa61206739fb45e298736a13b7088c
khaled <kzack-dev@hotmail.com>
2021-03-03 06:08:38 +0300

    build(release): remove release notes file after updating

--- release.sh
+++ release.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+release_notes_path=./.release-notes
+if [ -f release_notes_path] ; then
+  rm -rf release_notes_path
+fi
+
 tag=$(git tag --list | tail -n 1)
 
-changelog -t $tag -f ./.release-notes
-gh release create $tag -F ./.release-notes
+changelog -t $tag -f release_notes_path
+gh release create $tag -F release_notes_path
 
 # clean up
-rm -rf ./.release-notes
+rm -rf release_notes_path
